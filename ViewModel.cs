4696c3d0cdb7968ab1fd9c605221646fb52cb753
Христиан Белов <xristian1414@mail.ru>
2023-01-22 17:14:19 +0300

    Добавил код.

--- ViewModel.cs
+++ ViewModel.cs
@@ -1105,7 +1105,13 @@ namespace NeuralNetworkSnake
                     /*ChartValues1 = new ChartValues<double>(_simulation.GeneticLearningNoTeacher.MaxFitnessProgression);
                     ChartValues2 = new ChartValues<double>(_simulation.GeneticLearningNoTeacher.SelectionMethod.TargetHistory);*/
 
-                    int iterations = 1000;
+
+
+                    TestFunc();
+
+
+
+                    int iterations = 10000;
                     // initialize input and output values
                     double[][] input = new double[4][] {
                         new double[] {0, 0}, new double[] {0, 1},
@@ -1130,29 +1136,31 @@ namespace NeuralNetworkSnake
 
 
                     int inputDim = 2;
-                    int hiddenDim = 2;
+                    int hiddenDim = 8;
                     int numOutputClasses = 1;
                     DeviceDescriptor device = DeviceDescriptor.CPUDevice;
                     Variable inputVariable1 = Variable.InputVariable(new int[] { inputDim }, DataType.Float);
-                    Variable outputVariable1 = Variable.InputVariable(new int[] { hiddenDim }, DataType.Float);
-                    Variable inputVariable2 = Variable.InputVariable(new int[] { hiddenDim }, DataType.Float);
                     Variable outputVariable2 = Variable.InputVariable(new int[] { numOutputClasses }, DataType.Float);
 
                     var weightParam1 = new Parameter(new int[] { hiddenDim, inputDim }, DataType.Float, 1, device, "w");
                     var biasParam1 = new Parameter(new int[] { hiddenDim }, DataType.Float, 0, device, "b");
-                    var classifierOutput0 = CNTKLib.Sigmoid(CNTKLib.Times(weightParam1, inputVariable1) + biasParam1);
+                    var classifierOutput0 = CNTKLib.Sigmoid(CNTKLib.Plus(CNTKLib.Times(weightParam1, inputVariable1), biasParam1));
 
                     var weightParam2 = new Parameter(new int[] { numOutputClasses, hiddenDim }, DataType.Float, 1, device, "ww");
                     var biasParam2 = new Parameter(new int[] { numOutputClasses }, DataType.Float, 0, device, "bb");
-                    var classifierOutput1 = CNTKLib.Sigmoid(CNTKLib.Times(weightParam2, classifierOutput0) + biasParam2);
+                    //var classifierOutput1 = CNTKLib.Sigmoid(CNTKLib.Times(weightParam2, classifierOutput0) + biasParam2);
+                    var classifierOutput1 = CNTKLib.Sigmoid(CNTKLib.Plus(CNTKLib.Times(weightParam2, classifierOutput0), biasParam2));
 
-                    var loss = CNTKLib.CrossEntropyWithSoftmax(classifierOutput1, outputVariable2);
+
+
+                    //var loss = CNTKLib.CrossEntropyWithSoftmax(classifierOutput1, outputVariable2);
+                    var loss = CNTKLib.BinaryCrossEntropy(classifierOutput1, outputVariable2);
                     var evalError = CNTKLib.ClassificationError(classifierOutput1, outputVariable2);
 
                     // prepare for training
                     CNTK.TrainingParameterScheduleDouble learningRatePerSample = new CNTK.TrainingParameterScheduleDouble(0.01, 1);
                     IList<Learner> parameterLearners = new List<Learner>() { Learner.SGDLearner(classifierOutput1.Parameters(), learningRatePerSample) };
-                    var trainer = Trainer.CreateTrainer(classifierOutput1, loss, evalError, parameterLearners);
+                    var trainer = Trainer.CreateTrainer(classifierOutput1, loss, loss, parameterLearners);
 
                     float[] inputValuesArr = new float[input.Length * input[0].Length];
                     for (int i = 0; i < input.Length; i++)
@@ -1200,6 +1208,11 @@ namespace NeuralNetworkSnake
                     NDShape nDShape = new NDShape(1, 4);
                     Function logisticModel = CreateLogisticModel(Variable.InputVariable(nDShape, DataType.Float), 4);
                     LogisticRegression.TrainAndEvaluate(DeviceDescriptor.CPUDevice);
+
+
+
+
+                    
                 }, (obj) => true);
             }
         }
@@ -1213,8 +1226,130 @@ namespace NeuralNetworkSnake
                 CNTKLib.SentinelValueForInferParamInitRank, 1));
             Function z = CNTKLib.Plus(bias, CNTKLib.Times(weights, input));
             Function logisticClassifier = CNTKLib.Sigmoid(z, "LogisticClassifier");
-            //logisticClassifier.Inputs
             return logisticClassifier;
+        }
+        private void TestFunc()
+        {
+            var inputs = new[] { new[] { 0.0f, 0.0f }, new[] { 1.0f, 0.0f }, new[] { 0.0f, 1.0f }, new[] { 1.0f, 1.0f } };
+            var expected = new[] { new[] { 0.0f }, new[] { 1.0f }, new[] { 1.0f }, new[] { 0.0f } };
+
+            var device = DeviceDescriptor.CPUDevice;
+
+            const int inputDimensions = 2;
+            const int hiddenDimensions = 2;
+            const int outputDimensions = 1;
+
+            double initMin = -0.15;
+            double initMax = 0.15;
+            double normalMean = 0.0;
+            double standartDev = 0.25;
+            Random random = new Random();
+            uint seed = (uint)random.Next(1, 10000);
+
+            var inputVariable = Variable.InputVariable(new[] { inputDimensions }, DataType.Float);
+            var outputVariable = Variable.InputVariable(new[] { outputDimensions }, DataType.Float);
+
+            //var hiddenWeights = new Parameter(NDArrayView.RandomUniform<float>(new[] { hiddenDimensions, inputDimensions }, initMin, initMax, 1, device));
+            //var hiddenBias = new Parameter(NDArrayView.RandomUniform<float>(new[] { hiddenDimensions }, initMin, initMax, 1, device));
+            var hiddenWeights = new Parameter(NDArrayView.RandomNormal<float>(new[] { hiddenDimensions, inputDimensions }, normalMean, standartDev, seed++, device));
+            var hiddenBias = new Parameter(NDArrayView.RandomNormal<float>(new[] { hiddenDimensions }, normalMean, standartDev, seed++, device));
+            var hidden = CNTKLib.Sigmoid(CNTKLib.Plus(hiddenBias, CNTKLib.Times(hiddenWeights, inputVariable)));
+
+            //var outWeights = new Parameter(NDArrayView.RandomUniform<float>(new[] { outputDimensions, hiddenDimensions }, initMin, initMax, 1, device)); 
+            //var outBias = new Parameter(NDArrayView.RandomUniform<float>(new[] { outputDimensions }, initMin, initMax, 1, device));
+            var outWeights = new Parameter(NDArrayView.RandomNormal<float>(new[] { outputDimensions, hiddenDimensions }, normalMean, standartDev, seed++, device));
+            var outBias = new Parameter(NDArrayView.RandomNormal<float>(new[] { outputDimensions }, normalMean, standartDev, seed++, device));
+            var prediction = CNTKLib.Sigmoid(CNTKLib.Plus(outBias, CNTKLib.Times(outWeights, hidden)));
+
+
+
+            NDArrayView hiddenWeightsArrayViewBefore = hiddenWeights.Value();
+            Value hiddenWeightValueBefore = new Value(hiddenWeightsArrayViewBefore);
+            IList<IList<float>> hiddenWeightDataBefore = hiddenWeightValueBefore.GetDenseData<float>(hiddenWeights);
+
+            NDArrayView hiddenBiasArrayViewBefore = hiddenBias.Value();
+            Value hiddenBiasValueBefore = new Value(hiddenBiasArrayViewBefore);
+            IList<IList<float>> hiddenBiasDataBefore = hiddenBiasValueBefore.GetDenseData<float>(hiddenBias);
+
+            NDArrayView outWeightsArrayViewBefore = outWeights.Value();
+            Value outWeightsValueBefore = new Value(outWeightsArrayViewBefore);
+            IList<IList<float>> outWeightsDataBefore = outWeightsValueBefore.GetDenseData<float>(outWeights);
+
+            NDArrayView outBiasArrayViewBefore = outBias.Value();
+            Value outBiasValueBefore = new Value(outBiasArrayViewBefore);
+            IList<IList<float>> outBiasDataBefore = outBiasValueBefore.GetDenseData<float>(outBias);
+
+
+            var trainingLoss = CNTKLib.BinaryCrossEntropy(prediction, outputVariable);
+
+            /*var schedule = new TrainingParameterScheduleDouble(0.05);
+            var momentum = CNTKLib.MomentumAsTimeConstantSchedule(new DoubleVector(new[] { 1.0, 10.0, 100.0 }));
+            var learner = CNTKLib.AdamLearner(new ParameterVector(prediction.Parameters().Select(o => o).ToList()), schedule, momentum);
+            var trainer = CNTKLib.CreateTrainer(prediction, trainingLoss, trainingLoss, new LearnerVector(new[] { learner }));*/
+
+            TrainingParameterScheduleDouble learningRatePerSample = new TrainingParameterScheduleDouble(0.01, 1);
+            IList<Learner> parameterLearners = new List<Learner>() { Learner.SGDLearner(prediction.Parameters(), learningRatePerSample) };
+            var trainer = Trainer.CreateTrainer(prediction, trainingLoss, trainingLoss, parameterLearners);
+
+            const int minibatchSize = 40;
+
+            var features = new float[minibatchSize][];
+            var labels = new float[minibatchSize][];
+
+            const int numMinibatchesToTrain = 4000;
+            var trainIndex = 0;
+
+            for (var i = 0; i < numMinibatchesToTrain; ++i)
+            {
+                for (var j = 0; j < minibatchSize; ++j)
+                {
+                    features[j] = inputs[trainIndex];
+                    labels[j] = expected[trainIndex];
+                    trainIndex = (trainIndex + 1) % inputs.Length;
+                }
+                var batchInput = Value.Create(new[] { inputDimensions }, features.Select(o => new NDArrayView(new[] { inputDimensions }, o, device)), device);
+                var batchLabels = Value.Create(new[] { outputDimensions }, labels.Select(o => new NDArrayView(new[] { outputDimensions }, o, device)), device);
+                var minibatchBindings = new Dictionary<Variable, Value>
+                            {
+                                {inputVariable, batchInput},
+                                {outputVariable, batchLabels}
+                            };
+
+                trainer.TrainMinibatch(minibatchBindings, true, device);
+            }
+
+            float[] inputValuesArr = new float[inputs.Length * inputs[0].Length];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                for (int k = 0; k < inputs[i].Length; k++)
+                {
+                    inputValuesArr[i * inputs[i].Length + k] = inputs[i][k];
+                }
+            }
+            Value inputValues = Value.CreateBatch<float>(new int[] { inputDimensions }, inputValuesArr, device);
+            var inputDataMap = new Dictionary<Variable, Value>() { { inputVariable, inputValues } };
+            var outputDataMap = new Dictionary<Variable, Value>() { { prediction.Output, null } };
+            prediction.Evaluate(inputDataMap, outputDataMap, device);
+            var outputValue = outputDataMap[prediction.Output];
+            IList<IList<float>> actualLabelSoftMax = outputValue.GetDenseData<float>(prediction.Output);
+
+
+            NDArrayView hiddenWeightsArrayViewAfter = hiddenWeights.Value();
+            Value hiddenWeightValueAfter = new Value(hiddenWeightsArrayViewAfter);
+            IList<IList<float>> hiddenWeightDataAfter = hiddenWeightValueAfter.GetDenseData<float>(hiddenWeights);
+
+            NDArrayView hiddenBiasArrayViewAfter = hiddenBias.Value();
+            Value hiddenBiasValueAfter = new Value(hiddenBiasArrayViewAfter);
+            IList<IList<float>> hiddenBiasDataAfter = hiddenBiasValueAfter.GetDenseData<float>(hiddenBias);
+
+            NDArrayView outWeightsArrayViewAfter = outWeights.Value();
+            Value outWeightsValueAfter = new Value(outWeightsArrayViewAfter);
+            IList<IList<float>> outWeightsDataAfter = outWeightsValueAfter.GetDenseData<float>(outWeights);
+
+            NDArrayView outBiasArrayViewAfter = outBias.Value();
+            Value outBiasValueAfter = new Value(outBiasArrayViewAfter);
+            IList<IList<float>> outBiasDataAfter = outBiasValueAfter.GetDenseData<float>(outBias);
+            int y = 0;
         }
     }
 }
